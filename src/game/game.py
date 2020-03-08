1de850d0b21b118e115cff5d63ce2fbd742b254d
Neboer <rubinposter@gmail.com>
2020-03-08 10:46:53 +0800

    用户行为历史记录改为操作计数，同时完善game类的实现，包括其初始化过程。

--- src/game/game.py
+++ src/game/game.py
@@ -1,5 +1,6 @@
 from src.game.card import Card, CardColor, CardType
 from random import shuffle
+from src.game.player import Player
 
 
 class Game:
@@ -9,6 +10,33 @@ class Game:
     current_take_turns_positive = True  # 游戏正向进行，下一个出牌的玩家编号应该为(当前编号+1或2)%人数。
     current_count_of_cards_need_to_draw = 0  # 加牌叠加的数量
     current_card = None  # 当前状态位于牌堆顶的卡片
+
+    # 牌总是从后向前pop的，也就是优先弹出index较大的。如果牌不够了，就先pop全部然后洗牌再pop。
+    def _pop_cards(self, number):
+        if len(self.card_pool) < number:
+            part_one = self.card_pool
+            self.shuffle()
+            part_two = self.card_pool[-(number - len(part_one)):]
+            del self.card_pool[-(number - len(part_one)):]
+            return part_one + part_two
+        elif len(self.card_pool) == number:
+            part = self.card_pool
+            self.shuffle()
+            return part
+        else:
+            part = self.card_pool[-number:]
+            del self.card_pool[-number:]
+            return part
+
+    # 创建一个游戏。
+    def __init__(self, player_count, draw_cards):  # 传入初始玩家应该拥有手牌的数量
+        self.player_list = [Player(self, seat) for seat in range(1, player_count + 1)]
+        self.shuffle()
+        for player in self.player_list:
+            player.cards = self._pop_cards(draw_cards)  # 发牌
+        self.current_player_index = 1
+        self.current_take_turns_positive = True
+        self.current_count_of_cards_need_to_draw = 1
 
     def _next_player(self, value=1):
         if self.current_take_turns_positive:
@@ -30,8 +58,17 @@ class Game:
         elif card.type == CardType.turn:
             self.current_take_turns_positive = not self.current_take_turns_positive
         elif card.type == CardType.drawTwo:
-            self.current_count_of_cards_need_to_draw += 2
+            self.current_count_of_cards_need_to_draw += 2 if self.current_count_of_cards_need_to_draw != 1 else 1
         elif card.type == CardType.drawFour:
-            self.current_count_of_cards_need_to_draw += 4
+            self.current_count_of_cards_need_to_draw += 4 if self.current_count_of_cards_need_to_draw != 1 else 3
         # 对于ChangeColor和基本牌，我们什么也不需要做。
+        self._next_player()
+
+    def draw(self):
+        prepare_cards = self._pop_cards(self.current_count_of_cards_need_to_draw)
+        self.current_count_of_cards_need_to_draw = 1  # 用户摸牌后，牌局总需要摸牌的数量归1
+        # 玩家摸牌后，对局不继续进行，等待他的下一步操作。
+        return prepare_cards
+
+    def go(self):
         self._next_player()
--- src/game/player.py
+++ src/game/player.py
@@ -6,18 +6,26 @@ from src.game.card import Card
 class Player:
     cards = []  # type: List[Card]
     seat = 0  # 玩家在房间中的位置
-    operation = []  # 玩家在当前回合进行的操作列表。
+    operation_count = 0  # 玩家在当前回合进行的操作数量
     game = None  # type: Game # 玩家通过game属性访问自己所处的游戏实例。
+
+    def __init__(self, game, seat):
+        self.game = game
+        self.seat = seat
 
     def Put(self, card):  # type: (Card) -> None
         self.cards.remove(card)
         self.game.put(card)
+        self.operation_count = 0
         return
 
     def Draw(self):
         card = self.game.draw()
         self.cards.append(card)
+        self.operation_count += 1
         return card  # 将用户抽到的卡片返回给调用者
 
     def Go(self):
         self.game.go()  # 用户不出牌。
+        self.operation_count = 0
+        return
